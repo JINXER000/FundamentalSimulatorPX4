86dd1d62cb1b1b21864642039945c06ff9c42ba6
jinxer000 <czp7914@126.com>
2021-04-30 11:18:43 +0800

    add yaw control to .py

--- control_scripts/multirotor_communication_manual.py
+++ control_scripts/multirotor_communication_manual.py
@@ -185,7 +185,8 @@ class Communication:
  
     def cmd_pose_enu_callback(self, msg):
         self.coordinate_frame = 1
-        self.target_motion = self.construct_target(x=msg.position.x,y=msg.position.y,z=msg.position.z)
+        yaw_tgt= self.q2yaw(msg.orientation)
+        self.target_motion = self.construct_target(x=msg.position.x,y=msg.position.y,z=msg.position.z,yaw=yaw_tgt)
         
     def cmd_vel_flu_callback(self, msg):
         self.hover_state_transition(msg.linear.x, msg.linear.y, msg.linear.z, msg.angular.z)
